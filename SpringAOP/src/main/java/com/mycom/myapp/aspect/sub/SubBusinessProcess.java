2e963200cf801e5d95a60ed1052ae807a50ab810
Jimin Byun <fraise3838@naver.com>
2025-04-01 20:26:44 +0900

    Spring AOP

---
+++ SpringAOP/src/main/java/com/mycom/myapp/aspect/sub/SubBusinessProcess.java
@@ -0,0 +1,11 @@
+package com.mycom.myapp.aspect.sub;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SubBusinessProcess {
+	
+	public void bp() {
+		System.out.println("SubBusinessProcess : bp() !!");
+	}
+}
